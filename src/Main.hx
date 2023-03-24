701b466254d4b59416da94aa0eb0eae5b3aed981
Facilitated <47868851+FacilitatedIsTaken@users.noreply.github.com>
2023-03-24 14:24:57 -0300

    initial commit

---
+++ src/Main.hx
@@ -0,0 +1,17 @@
+import hxd.res.Font;
+import hxd.res.DefaultFont;
+import h2d.Text;
+
+class Main extends hxd.App {    
+    override function init() {
+
+    }
+    
+    override function update(dt:Float) {
+
+    }
+
+    static function main() {
+        new Main();
+    }
+}
